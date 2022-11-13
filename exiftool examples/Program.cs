8d1d401e8a2361bff22e919a1557a8abce027ad9
KurtLarsen <github@monkymail.dk>
2022-11-13 19:30:19 +0100

    Changed CmdRun and PhotoTools

---
+++ exiftool examples/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NSKurtsTools;
+using static NSKurtsTools.KurtsTools;
+
+CmdRunSetUp x = new(){Command = "cmd.exe",Arguments = "/c dir",WorkingDirectory = @"c:\"};
+CmdRunResult result=CmdRun(x);
+Console.WriteLine($"result.ExitCode: <{result.ExitCode}>");
+Console.WriteLine($"result.ErrOut:\n{result.ErrOut}");
+Console.WriteLine($"result.StdOut:\n{result.StdOut}");
